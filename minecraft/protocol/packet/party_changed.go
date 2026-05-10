a3adf81df940523f4aa2559a1aa3b4747055ded2
Yeah114 <1246517085@qq.com>
2026-05-10 12:04:25 +0800

    packet/party_changed.go: Simplify PartyInfo marshaling

--- minecraft/protocol/packet/party_changed.go
+++ minecraft/protocol/packet/party_changed.go
@@ -6,10 +6,7 @@ import (
 
 // PartyChanged is sent by the client to the server to indicate that the player's party ID has changed.
 type PartyChanged struct {
-	// PartyID is the optional party identifier.
-	PartyID protocol.Optional[string]
-	// PartyLeader is if the client is the new party leader or not. This field was added in v1.26.20.26.
-	PartyLeader bool
+	PartyInfo protocol.Optional[PartyInfo]
 }
 
 // ID ...
@@ -18,8 +15,20 @@ func (*PartyChanged) ID() uint32 {
 }
 
 func (pk *PartyChanged) Marshal(io protocol.IO) {
-	protocol.OptionalFunc(io, &pk.PartyID, io.String)
+	protocol.OptionalMarshaler(io, &pk.PartyInfo)
+}
+
+// PartyInfo represents the information of the client's role in a party.
+type PartyInfo struct {
+	// PartyID is the party identifier.
+	PartyID string
+	// PartyLeader is if the client is the new party leader or not. This field was added in v1.26.20.26.
+	PartyLeader bool
+}
+
+func (x *PartyInfo) Marshal(io protocol.IO) {
+	io.String(&x.PartyID)
 	if io.Protocol() >= protocol.Protocol1v26v20v26 {
-		io.Bool(&pk.PartyLeader)
+		io.Bool(&x.PartyLeader)
 	}
 }
