33c88598bdcee146146aaca9ed018b6bce1fc8a6
Yeah114 <1246517085@qq.com>
2026-05-10 11:26:09 +0800

    packet/party_changed.go: Add 1.26.20.26 condition for PartyLeader

--- minecraft/protocol/packet/party_changed.go
+++ minecraft/protocol/packet/party_changed.go
@@ -6,7 +6,10 @@ import (
 
 // PartyChanged is sent by the client to the server to indicate that the player's party ID has changed.
 type PartyChanged struct {
-	PartyInfo protocol.Optional[PartyInfo]
+	// PartyID is the optional party identifier.
+	PartyID protocol.Optional[string]
+	// PartyLeader is if the client is the new party leader or not. This field was added in v1.26.20.26.
+	PartyLeader bool
 }
 
 // ID ...
@@ -15,18 +18,8 @@ func (*PartyChanged) ID() uint32 {
 }
 
 func (pk *PartyChanged) Marshal(io protocol.IO) {
-	protocol.OptionalMarshaler(io, &pk.PartyInfo)
-}
-
-// PartyInfo represents the information of the client's role in a party.
-type PartyInfo struct {
-	// PartyID is the party identifier.
-	PartyID string
-	// PartyLeader is if the client is the new party leader or not.
-	PartyLeader bool
-}
-
-func (x *PartyInfo) Marshal(io protocol.IO) {
-	io.String(&x.PartyID)
-	io.Bool(&x.PartyLeader)
+	protocol.OptionalFunc(io, &pk.PartyID, io.String)
+	if io.Protocol() >= protocol.Protocol1v26v20v26 {
+		io.Bool(&pk.PartyLeader)
+	}
 }
