6857f0819d0cae55bfd7d8be5b233b2aa012000e
Yeah114 <1246517085@qq.com>
2026-05-17 09:35:31 +0800

    gophertunnel[Multiversion]: Add legacy item component format

--- minecraft/protocol/packet/item_registry.go
+++ minecraft/protocol/packet/item_registry.go
@@ -1,6 +1,7 @@
 package packet
 
 import (
+	"github.com/sandertv/gophertunnel/minecraft/nbt"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
@@ -8,13 +9,15 @@ import (
 // components to a custom item. This packet was formerly known as the ItemComponent packet before 1.21.60,
 // which did not include item definitions but only the components.
 //
-// Added: v1.21.60
+// Added: v1.16.100
+// Changed: v1.21.60, renamed from ItemComponent and expanded to include item definitions.
 type ItemRegistry struct {
 	// Items is a list of all items with their legacy IDs which are available in the game. Failing to send any
 	// of the items that are in the game will crash mobile clients. Any custom components are also attached to
 	// the items in this list.
 	//
-	// Added: v1.21.60
+	// Added: v1.16.100
+	// Changed: v1.21.60, expanded from component data to full item entries with runtime IDs and versions.
 	Items []protocol.ItemEntry
 }
 
@@ -24,5 +27,12 @@ func (*ItemRegistry) ID() uint32 {
 }
 
 func (pk *ItemRegistry) Marshal(io protocol.IO) {
+	if io.Protocol() < protocol.Protocol1v21v60 {
+		protocol.FuncSlice(io, &pk.Items, func(x *protocol.ItemEntry) {
+			io.String(&x.Name)
+			io.NBT(&x.Data, nbt.NetworkLittleEndian)
+		})
+		return
+	}
 	protocol.Slice(io, &pk.Items)
 }
